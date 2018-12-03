ebb72452c501d2f8ad4ee59f4e07289d74242f49
Philipp Frenzel <philipp@frenzel.net>
2018-12-03 11:01:18 +0100

    Update hints for the creation of the url

--- properties/properties.js
+++ properties/properties.js
@@ -1,15 +1,14 @@
+define([
 
-define( [
+    'jquery',
+    'qlik',
+    'ng!$q',
+    'ng!$http'
 
-	'jquery',
-	'qlik',
-	'ng!$q',
-	'ng!$http'
-
-], function ($, qlik, $q, $http) {
+], function($, qlik, $q, $http) {
     'use strict';
-	//Define the current application
-	var app = qlik.currApp();
+    //Define the current application
+    var app = qlik.currApp();
 
     // *****************************************************************************
     // Appearance Section
@@ -17,49 +16,49 @@ define( [
     var appearanceSection = {
         uses: "settings"
     };
-	
-	
-	// *****************************************************************************
+
+
+    // *****************************************************************************
     // Add Ons Section
     // *****************************************************************************
-	var addOnsSection = {
-		uses: "addons",
-			items: {  
-				  dataHandling: {  
-					   uses: "dataHandling"  
-					  }  
-				 }  
-	};
-	
-	
-	// *****************************************************************************
+    var addOnsSection = {
+        uses: "addons",
+        items: {
+            dataHandling: {
+                uses: "dataHandling"
+            }
+        }
+    };
+
+
+    // *****************************************************************************
     // Options Section
     // *****************************************************************************
 
-	var d_url = {
-			ref: "d_url",
-			type: "string",
-			label: "Dynamic URL format ='http://...' ",
-			expression: "optional",
-			defaultValue: ""
-	};
-	
-	var Options = {
-		type:"items",
-		label:"Options",
-		items: {
-			d_url:d_url
-		}
-	};
-	
-	
+    var d_url = {
+        ref: "d_url",
+        type: "string",
+        label: "Jedox Server URL in the format http://localhost' - you need to grep the url from the properties Dialog within the 'Reports' section.",
+        expression: "optional",
+        defaultValue: "http://localhost"
+    };
+
+    var Options = {
+        type: "items",
+        label: "Options",
+        items: {
+            d_url: d_url
+        }
+    };
+
+
     // *****************************************************************************
     // Main property panel definition
     // ~~
     // Only what's defined here will be returned from properties.js
     // *****************************************************************************
-	  
-	//******************************************************************************
+
+    //******************************************************************************
 
     return {
         type: "items",
@@ -67,9 +66,9 @@ define( [
         items: {
             Options: Options,
             appearance: appearanceSection,
-			addons: addOnsSection
+            addons: addOnsSection
 
         }
     };
 
-} );
+});
